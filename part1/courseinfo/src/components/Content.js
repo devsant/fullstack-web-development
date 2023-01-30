749b4629e7ddf9a51bef4103799de401b00ca5bc
devsant <nosferatu90@gmail.com>
2023-01-29 22:11:19 -0300

    part1 step3 and step4

--- part1/courseinfo/src/components/Content.js
+++ part1/courseinfo/src/components/Content.js
@@ -3,9 +3,9 @@ import Part from "./Part";
 const Content = (props) => {
 	return (
 		<div>
-			<Part content={props.content1} part={props.part1} />
-			<Part content={props.content2} part={props.part2} />
-			<Part content={props.content3} part={props.part3} />
+			<Part content={props.parts[0].name} part={props.parts[0].exercises} />
+			<Part content={props.parts[1].name} part={props.parts[1].exercises} />
+			<Part content={props.parts[2].name} part={props.parts[2].exercises} />
 		</div>
 	);
 };
